5b5b1248fdc95818502f86e8a01c15719512b662
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-03-29 21:23:37 +1100

    Version 4
    Server Working

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour
+public class Player : Photon.MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PhotonView photonView;
+    public Rigidbody rb;
+    public Animator anim;
+    public GameObject PlayerCamera;
+    public SpriteRenderer sr;
+    public TextAlignment PlayerNameText;
+
+    public bool IsGrounded = false;
+    public float MoveSpeed;
+    public float JumpForce;
+
+    private void Awake()
     {
-        
+        if(photonView.isMine)
+        {
+            PlayerCamera.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (photonView.isMine)
+        {
+            CheckInput();
+        }
+    }
+    private void CheckInput()
+    {
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        transform.position += move * MoveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sr.flipX = true;   
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sr.flipX = false;
+        }
     }
 }

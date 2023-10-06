382007ee7635cec1f48c9fab37fefc30e1f8b8af
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-10-06 11:55:34 +1100

    Game UI
    - Menu Screen Setup and functionality

---
+++ Assets/Scripts/MenuCameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class MenuCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private float duration;
+
+    public void LookAt(Transform target)
+    {
+        transform.DOLookAt(target.position, duration);
+    }
+}
